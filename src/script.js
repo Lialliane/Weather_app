c79349d010ba6e4fc1fb130beed7a10c723d9a70
Lialliane <lojain01k@gmail.com>
2024-02-18 20:52:48 +0300

    Updated the first weather to appear when the page is opened. Added the search function.
    When the user opens the app the first city data to appear will be the same as their city, according to their IP address at least.
    The search function updates the weather information based on the city the user writes.

--- src/script.js
+++ src/script.js
@@ -1,13 +1,115 @@
 let ipURL = 'http://ip-api.com/json/';
-let timeURL = "http://worldtimeapi.org/api/";
+let key = '051f2c33e4acf0eb6efa3eaab379t3o3';
+var city = '';
+var timeURL = '';
+
 axios.get(ipURL).then(function(response){
-    console.log(response)
+    console.log(response);
+    city = response.data.city;
+    let weatherUrlShort = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+    axios.get(weatherUrlShort).then(updateCity);
+
 });
+
+function updateCity(response){
+    console.log(response);
+    let city = document.querySelector("#city-name");
+    let currentTemp = document.querySelector("#current-temp");
+    let weatherCondition = document.querySelector("#weather-condition");
+    let currentWeatherIcon = document.querySelector("#current-weather-icon");
+    let humidity = document.querySelector("#humidity");
+    let pressure = document.querySelector("#pressure");
+    let windSpeed = document.querySelector("#wind-speed");
+    let feelsLike = document.querySelector("#feels-like");
+    city.innerHTML = response.data.city;
+    currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°C";
+    weatherCondition.innerHTML = response.data.condition.description;
+    currentWeatherIcon.setAttribute("src", response.data.condition.icon_url);
+    humidity.innerHTML = response.data.temperature.humidity + "%";
+    pressure.innerHTML = response.data.temperature.pressure;
+    windSpeed.innerHTML = response.data.wind.speed + ' km/h';
+    feelsLike.innerHTML = Math.round(response.data.temperature.feels_like) + "°C";
+
+    let weatherUrlLong = `https://api.shecodes.io/weather/v1/forecast?lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}&key=${key}&units=metric`;
+    axios.get(weatherUrlLong).then(updateDetailsAndWeek);
+}
+
+function updateDetailsAndWeek(response){
+    console.log(response);
+    let maxTemp = document.querySelector("#max-temp");
+    let minTemp = document.querySelector("#min-temp");
+    maxTemp.innerHTML = Math.round(response.data.daily[0].temperature.maximum) + "°C";
+    minTemp.innerHTML = Math.round(response.data.daily[0].temperature.minimum) + "°C";
+
+    let weekWeather = document.querySelector("#week-weather-forecast");
+    let dayOneTemp = document.querySelector("#day-1-temp");
+    let dayOneIcon = document.querySelector("#day-1-icon");
+    let dayTwoTemp = document.querySelector("#day-2-temp");
+    let dayTwoDate = document.querySelector("#day-2-date");
+    let dayTwoIcon = document.querySelector("#day-2-icon");
+    let dayThreeTemp = document.querySelector("#day-3-temp");
+    let dayThreeDate = document.querySelector("#day-3-date");
+    let dayThreeIcon = document.querySelector("#day-3-icon");
+
+    dayOneTemp.innerHTML = Math.round(response.data.daily[1].temperature.day) + "°C";
+    dayOneIcon.setAttribute("src", response.data.daily[1].condition.icon_url);
+    dayTwoTemp.innerHTML = Math.round(response.data.daily[2].temperature.day) + "°C";
+    dayTwoDate.innerHTML = formatDate(1, false);
+    dayTwoIcon.setAttribute("src", response.data.daily[2].condition.icon_url);
+    dayThreeTemp.innerHTML = Math.round(response.data.daily[3].temperature.day) + "°C";
+    dayThreeDate.innerHTML = formatDate(2, false);
+    dayThreeIcon.setAttribute("src", response.data.daily[3].condition.icon_url);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    city = document.querySelector("#search-field").value;
+    let weatherUrlShort = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+    axios.get(weatherUrlShort).then(updateCity);
+})
+
+var currentDate = document.querySelector("#current-date");
+var currentTime = document.querySelector("#current-time");
+currentDate.innerHTML = formatDate();
+currentTime.innerHTML = formatTime();
+
+function formatDate(dayOffSet = 0, toggleFomrat = true){
+    var dateData = new Date();
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+;
+    var day = dateData.getDay();
+    var date = dateData.getDate()+dayOffSet;
+    var month = dateData.getMonth();
+    var year = dateData.getFullYear();
+    if(toggleFomrat){
+
+        return `${dayNames[day]}, ${date}/${month}/${year}`
+    }
+    else
+        return `${monthNames[month]} ${date}`;
+}
+
+function formatTime(){
+    var dateData = new Date();
+    var timeReg = /[(].*[)]/;
+    var timeZone = dateData.toString().match(timeReg).toString().replace("(", "").replace(")", "");
+    var hours = dateData.getHours();
+    var minutes = dateData.getMinutes();
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if (hours < 10){
+        hours = "0" + hours;
+    }
+    return  `${hours}:${minutes} ${timeZone}`;
+}
+
 
 let expandButton = document.querySelector(".additional-details-button");
 let additionalDetails = document.querySelector("#additional-details-container");
 var open = false;
-
 
 expandButton.addEventListener("click", function(){
     open = !open;
