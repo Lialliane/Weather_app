cb1d67ebc8c7912181c2b601092d708fd20334f6
Lialliane <lojain01k@gmail.com>
2024-02-19 19:38:48 +0300

    Added error handling and made cutom error messages to appear for each case.

--- src/script.js
+++ src/script.js
@@ -1,37 +1,44 @@
 let ipURL = 'http://ip-api.com/json/';
 let key = '051f2c33e4acf0eb6efa3eaab379t3o3';
 var city = '';
-var timeURL = '';
 
 axios.get(ipURL).then(function(response){
     console.log(response);
     city = response.data.city;
     let weatherUrlShort = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
-    axios.get(weatherUrlShort).then(updateCity);
+    console.log(weatherUrlShort);
+    axios.get(weatherUrlShort).then(updateCity).catch(showErrorAfterSubmit);
+    });
 
-});
+
 
 function updateCity(response){
     console.log(response);
-    let city = document.querySelector("#city-name");
-    let currentTemp = document.querySelector("#current-temp");
-    let weatherCondition = document.querySelector("#weather-condition");
-    let currentWeatherIcon = document.querySelector("#current-weather-icon");
-    let humidity = document.querySelector("#humidity");
-    let pressure = document.querySelector("#pressure");
-    let windSpeed = document.querySelector("#wind-speed");
-    let feelsLike = document.querySelector("#feels-like");
-    city.innerHTML = response.data.city;
-    currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°C";
-    weatherCondition.innerHTML = response.data.condition.description;
-    currentWeatherIcon.setAttribute("src", response.data.condition.icon_url);
-    humidity.innerHTML = response.data.temperature.humidity + "%";
-    pressure.innerHTML = response.data.temperature.pressure;
-    windSpeed.innerHTML = response.data.wind.speed + ' km/h';
-    feelsLike.innerHTML = Math.round(response.data.temperature.feels_like) + "°C";
+    console.log(response.data);
+     if(response.data.status=="not_found"){
+        showError("not-found");
+     }
+    else{
+        let city = document.querySelector("#city-name");
+        let currentTemp = document.querySelector("#current-temp");
+        let weatherCondition = document.querySelector("#weather-condition");
+        let currentWeatherIcon = document.querySelector("#current-weather-icon");
+        let humidity = document.querySelector("#humidity");
+        let pressure = document.querySelector("#pressure");
+        let windSpeed = document.querySelector("#wind-speed");
+        let feelsLike = document.querySelector("#feels-like");
+        city.innerHTML = response.data.city;
+        currentTemp.innerHTML = Math.round(response.data.temperature.current) + "°C";
+        weatherCondition.innerHTML = response.data.condition.description;
+        currentWeatherIcon.setAttribute("src", response.data.condition.icon_url);
+        humidity.innerHTML = response.data.temperature.humidity + "%";
+        pressure.innerHTML = response.data.temperature.pressure;
+        windSpeed.innerHTML = response.data.wind.speed + ' km/h';
+        feelsLike.innerHTML = Math.round(response.data.temperature.feels_like) + "°C";
 
-    let weatherUrlLong = `https://api.shecodes.io/weather/v1/forecast?lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}&key=${key}&units=metric`;
-    axios.get(weatherUrlLong).then(updateDetailsAndWeek);
+        let weatherUrlLong = `https://api.shecodes.io/weather/v1/forecast?lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}&key=${key}&units=metric`;
+        axios.get(weatherUrlLong).then(updateDetailsAndWeek).catch(showErrorAfterSubmit);
+     }
 }
 
 function updateDetailsAndWeek(response){
@@ -62,11 +69,30 @@ function updateDetailsAndWeek(response){
 }
 
 let form = document.querySelector("form");
+let searchField = document.querySelector("#search-field");
+
+searchField.addEventListener("keydown", function(event){
+    setTimeout(function(){
+        let input = document.querySelector("#search-field");
+        let error = document.querySelector(".error");
+        if(!input.validity.valid){
+            showError();
+            return;
+        }
+
+        else if (event.keyCode === 13)
+            return;
+        else {
+            error.innerHTML = "";
+            error.classList.remove("error-message");
+        }
+    },500);
+})
 form.addEventListener("submit", function(event){
     event.preventDefault();
     city = document.querySelector("#search-field").value;
     let weatherUrlShort = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
-    axios.get(weatherUrlShort).then(updateCity);
+    axios.get(weatherUrlShort).then(updateCity).catch(showErrorAfterSubmit);
 })
 
 var currentDate = document.querySelector("#current-date");
@@ -138,3 +164,35 @@ expandButton.addEventListener("click", function(){
 })
 
 
+function showError(error){
+    var errorMessage = '';
+    let input = document.querySelector("#search-field");
+    let errorElement = document.querySelector(".error");
+    console.log(input.validity);
+    console.log(error);
+
+     if(input.validity.valueMissing){
+        errorMessage = "You need to enter a city";
+    }
+    else if(input.validity.typeMismatch){
+        errorMessage = "Please enter a valid city name";
+    }
+    else if(input.validity.patternMismatch){
+        errorMessage = "Please enter letters only";
+    }
+
+    else if(error=="not-found"){
+        errorMessage = "Please enter a valid city name";
+    }
+
+    errorElement.classList.add("error-message");
+    errorElement.innerHTML = errorMessage;
+}
+
+function showErrorAfterSubmit(error){
+    var errorMessage = 'Sorry, something went wrong. Please try again';
+    let errorElement = document.querySelector(".error");
+
+    errorElement.classList.add("error-message");
+    errorElement.innerHTML = errorMessage;
+}
